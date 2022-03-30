0a0c1306a2c13d243ff80481a05d6c78ba78f6b0
SirAlyon <alessiosada04@gmail.com>
2022-03-30 10:53:30 +0200

    edit

--- snack-2/main.js
+++ snack-2/main.js
@@ -6,6 +6,9 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 const firstWord = prompt('Inserisci la prima parola:')
 const secondWord = prompt('Inserisci la seconda parola:')
 
+if (firstWord === "" || secondWord === ""){
+    alert('Per favore inserisci almeno un carattere')
+}
 
 if (firstWord.length > secondWord.length){
     console.log(`La più corta: ${secondWord} La più lunga: ${firstWord}`);
@@ -13,7 +16,4 @@ if (firstWord.length > secondWord.length){
     console.log(`La più corta: ${firstWord} La più lunga: ${secondWord}`)
 } else if (firstWord.length == secondWord.length){
     console.log(`Le due parole hanno la stessa lunghezza`)
-} else if (firstWord === "" || secondWord === ""){
-    alert('Per favore inserisci almeno un carattere')
-}
-
+} 
