33a5e09cfaa0d0c2997e1bb22a33cc0d0595a97d
SirAlyon <alessiosada04@gmail.com>
2022-03-30 10:23:20 +0200

    add isNan for firstNumber and secondNumber

--- snack-1/main.js
+++ snack-1/main.js
@@ -5,10 +5,14 @@ Il software stampa il maggiore. */
 const firstNumber = Number(prompt('Inserisci il primo numero:'));
 const secondNumber = Number(prompt('Inserisci il secondo numero:'));
 
+if (isNaN(firstNumber) || isNaN(secondNumber)){
+    alert('Per favore inserisci solo valori numerici!')
+}
+
 if (firstNumber > secondNumber){
     console.log(`Il primo numero è più grande: ${firstNumber}`);
 } else if (firstNumber < secondNumber){
     console.log(`Il secondo numero è più grande: ${secondNumber}`);
-} else {
+} else if (firstNumber == secondNumber){
     console.log(`I due numeri scelti sono uguali!`)
 }
