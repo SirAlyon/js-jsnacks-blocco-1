fbda8232501ed34d43bfc72d0a3a59c1c266ffd0
SirAlyon <alessiosada04@gmail.com>
2022-03-30 13:09:38 +0200

    add snack-5

---
+++ snack-5/main.js
@@ -0,0 +1,13 @@
+/* Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+ */
+
+let oddNumbers = []
+
+for (let i=0; i<6; i++){
+    let userNumbers = Number(prompt('Digita un numero:'))
+    if (userNumbers % 2 == 0){
+        oddNumbers.push(userNumbers)
+    }
+}
