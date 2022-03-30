7c72306e31eebc6a518f16ade470d87558533543
SirAlyon <alessiosada04@gmail.com>
2022-03-30 11:00:22 +0200

    add boolean variables

--- snack-2/main.js
+++ snack-2/main.js
@@ -6,14 +6,17 @@ Il software stampa prima la parola più corta, poi la parola più lunga. */
 const firstWord = prompt('Inserisci la prima parola:')
 const secondWord = prompt('Inserisci la seconda parola:')
 
-if (firstWord === "" || secondWord === ""){
+let boolean = true
+
+if (firstWord.length == 0 || secondWord.length == 0){
     alert('Per favore inserisci almeno un carattere')
+    boolean = false
 }
 
-if (firstWord.length > secondWord.length){
+if (firstWord.length > secondWord.length && boolean){
     console.log(`La più corta: ${secondWord} La più lunga: ${firstWord}`);
-} else if (firstWord.length < secondWord.length){
+} else if (firstWord.length < secondWord.length && boolean){
     console.log(`La più corta: ${firstWord} La più lunga: ${secondWord}`)
-} else if (firstWord.length == secondWord.length){
+} else if (firstWord.length == secondWord.length && boolean){
     console.log(`Le due parole hanno la stessa lunghezza`)
 } 
